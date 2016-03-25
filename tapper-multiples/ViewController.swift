7cc8f6abe5c084efb4fad847ca31352dbb83f862
Craig Newcomb <crgnewcomb@gmail.com>
2016-03-25 18:09:18 -0500

    Added Logo, game logic, etc. Basically everything to make the game function.

--- tapper-multiples/ViewController.swift
+++ tapper-multiples/ViewController.swift
@@ -9,17 +9,84 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    // This is supposed to dismiss keyboard
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        
+        self.view.endEditing(true)
+        
     }
+    
+    //Initialize Properties
+    
+    var multiple = 0
+    var maxMultiply = 5
+    var timesMultiply = 0
+    var sum = 0
+    
+    // Outlets
+    
+    @IBOutlet weak var PlayButton: UIButton!
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBOutlet weak var Logo: UIImageView!
+    @IBOutlet weak var TitleLabel: UILabel!
+    @IBOutlet weak var EnterText: UITextField!
+
+    @IBOutlet weak var EquationLabel: UILabel!
+    
+    @IBOutlet weak var MultiplyButton: UIButton!
+    
+    
+    // Actions
+    
+    @IBAction func onPlayButtonPressed(sender: AnyObject!) {
+        
+        if EnterText.text != nil && EnterText.text != "" {
+            multiple = Int(EnterText.text!)!
+        
+            Logo.hidden = true
+            TitleLabel.hidden = true
+            EnterText.hidden = true
+            PlayButton.hidden = true
+        
+            EquationLabel.hidden = false
+            MultiplyButton.hidden = false
+            
+            updateEquationlabel()
+        }
     }
-
-
+    
+    @IBAction func onMultiplyButtonPressed(sender: UIButton!) {
+        timesMultiply += 1
+        updateEquationlabel()
+        sum = sum + multiple
+        if isGameOver() {
+            restartGame()
+        }
+    }
+    
+    func updateEquationlabel() {
+        EquationLabel.text = "\(sum) + \(multiple) = \(sum + multiple)"
+    }
+    
+    func restartGame() {
+        
+        sum = 0
+        timesMultiply = 0
+        EnterText.text = ""
+        
+        Logo.hidden = false
+        TitleLabel.hidden = false
+        EnterText.hidden = false
+        PlayButton.hidden = false
+        
+        EquationLabel.hidden = true
+        MultiplyButton.hidden = true
+    }
+    
+    func isGameOver() -> Bool {
+        if timesMultiply >= maxMultiply {
+            return true
+        } else { return false }
+    }
 }
 
